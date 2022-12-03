de0b92ff988d2f359b88b405fde8d8b184815e8b
Tom Hare <tom@hare.me>
2022-12-03 22:42:24 +0000

    Refactored day 2 🎄

--- 2022/day2/index.js
+++ 2022/day2/index.js
@@ -3,125 +3,41 @@ const fs = require('fs');
 
 const data = fs.readFileSync(`./input.txt`, 'utf-8');
 
-const games = data.split('\n');
+const games = data.split('\n').map(game => game.replace(' ', ''));
 
-let score = 0;
-games.forEach(game => {
-  const [opponent, you] = game.split(' ');
+// Each game is a code which can be assigned a result.
+const part1Scores = {
+  AX: 4,
+  AY: 8,
+  AZ: 3,
+  BX: 1,
+  BY: 5,
+  BZ: 9,
+  CX: 7,
+  CY: 2,
+  CZ: 6,
+};
 
-  // You throw rock.
-  if (you === 'X') {
-    score++;
+const part2Scores = {
+  AX: 3,
+  AY: 4,
+  AZ: 8,
+  BX: 1,
+  BY: 5,
+  BZ: 9,
+  CX: 2,
+  CY: 6,
+  CZ: 7,
+};
 
-    // Rock draws rock.
-    if (opponent === 'A') {
-      score = score + 3;
-    }
-
-    // Rock defeats scissors.
-    if (opponent === 'C') {
-      score = score + 6;
-    }
-  }
-
-  // You throw paper.
-  if (you === 'Y') {
-    score = score + 2;
-
-    // Paper defeats rock.
-    if (opponent === 'A') {
-      score = score + 6;
-    }
-
-    // Paper draws paper.
-    if (opponent === 'B') {
-      score = score + 3;
-    }
-  }
-
-  // You throw scissors.
-  if (you === 'Z') {
-    score = score + 3;
-
-    // Scissors defeats paper.
-    if (opponent === 'B') {
-      score = score + 6;
-    }
-
-    // Scissors draws scissors.
-    if (opponent === 'C') {
-      score = score + 3;
-    }
-  }
-});
+// Get score for each game and tally.
+const calcScores = (scores, total = 0) => {
+  games.forEach(game => total += scores[game]);
+  return total;
+};
 
 // Print part one.
-console.log(score);
-
-
-let score2 = 0;
-games.forEach(game => {
-  const [opponent, result] = game.split(' ');
-
-  // Need to lose.
-  if (result === 'X') {
-
-    // Throw scissors to lose to rock.
-    if (opponent === 'A') {
-      score2 = score2 + 3;
-    }
-
-    // Throw rock to lose to paper.
-    if (opponent === 'B') {
-      score2 = score2 + 1;
-    }
-
-    // Throw paper to lose to scissors.
-    if (opponent === 'C') {
-      score2 = score2 + 2;
-    }
-  }
-
-  // Need to draw.
-  if (result === 'Y') {
-    score2 = score2 + 3;
-
-    // Throw rock to draw with rock.
-    if (opponent === 'A') {
-      score2 = score2 + 1;
-    }
-
-    // Throw paper to draw with paper.
-    if (opponent === 'B') {
-      score2 = score2 + 2;
-    }
-
-    // Throw scissors to draw with scissors.
-    if (opponent === 'C') {
-      score2 = score2 + 3;
-    }
-  }
-
-  // Need to win.
-  if (result === 'Z') {
-    score2 = score2 + 6;
-
-    // Throw paper to defeat rock.
-    if (opponent === 'A') {
-      score2 = score2 + 2;
-    }
-
-    // Throw scissors to defeat paper.
-    if (opponent === 'B') {
-      score2 = score2 + 3;
-    }
-
-    // Throw rock to defeat scissors.
-    if (opponent === 'C') {
-      score2 = score2 + 1;
-    }
-  }
-});
+console.log(calcScores(part1Scores));
 
 // Print part two.
-console.log(score2);
+console.log(calcScores(part2Scores));
