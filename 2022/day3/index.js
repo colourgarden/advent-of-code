f86b01ad735eda319378f811bd50710c63930bf4
Tom Hare <tom@hare.me>
2022-12-06 23:33:41 +0000

    Refactored day 3 🎄

--- 2022/day3/index.js
+++ 2022/day3/index.js
@@ -3,103 +3,60 @@ const fs = require('fs');
 
 const data = fs.readFileSync(`./input.txt`, 'utf-8');
 
-const mappings = {
-  a: 1,
-  b: 2,
-  c: 3,
-  d: 4,
-  e: 5,
-  f: 6,
-  g: 7,
-  h: 8,
-  i: 9,
-  j: 10,
-  k: 11,
-  l: 12,
-  m: 13,
-  n: 14,
-  o: 15,
-  p: 16,
-  q: 17,
-  r: 18,
-  s: 19,
-  t: 20,
-  u: 21,
-  v: 22,
-  w: 23,
-  x: 24,
-  y: 25,
-  z: 26,
+// Map letters to ASCII decimals and return priority.
+// a-z is range 97-122, A-Z is range is 65-90.
+// Priority runs a-z+A-Z. Maps to 1-26+27-52, hence capitals offset by 26.
+function mapLetterToPriority(letter) {
+  const offset = letter == letter.toLowerCase() ? 96 : 38;
+  return letter.charCodeAt(0) - offset;
 }
 
-let result = [];
-
-const bags = data.split('\n').map(bag => {
+// Return letter that is shared between each compartment in a bag.
+const sharedLettersInBags = data.split('\n').map(bag => {
   const compartment1 = bag.substring(0, bag.length / 2);
   const compartment2 = bag.substring(bag.length / 2);
-  // console.log(compartment1, compartment2);
 
   const set1 = new Set(compartment1.split(''))
   const set2 = new Set(compartment2.split(''))
 
-  for (let char of set1.values()){
-    if (set2.has(char)) result.push(char);
+  for (let char of set1.values()) {
+    if (set2.has(char)) return char;
   }
 });
 
-let score = 0;
-let score3 = 0;
+// Map shared letters to a priority value and tally them up.
+const score1 = sharedLettersInBags.map((char) => mapLetterToPriority(char)).reduce((a, c) => a + c, 0);
 
-result.forEach(char => {
-  if (char == char.toLowerCase()) {
-    score += mappings[char];
-  }
-  else {
-    score = score + (mappings[char.toLowerCase()] + 26);
-  }
-});
-
-// Log part one.
-console.log(score);
-
+// Print answer to part one.
+console.log(score1);
 
 
 function chunk (items, size) {
-  const chunks = []
-  items = [].concat(...items)
-
+  const chunks = [];
+  items = [].concat(...items);
   while (items.length) {
-    chunks.push(
-      items.splice(0, size)
-    )
+    chunks.push(items.splice(0, size));
   }
-
-  return chunks
+  return chunks;
 }
 
+// Split elves up into groups of three.
 const elves = data.split('\n');
 const groups = chunk(elves, 3);
 
-let common = [];
-groups.map(group => {
+// Find the common letter/item between the elves in each group.
+const commonGroupItem = groups.map(group => {
   const set1 = new Set(group[0].split(''))
   const set2 = new Set(group[1].split(''))
   const set3 = new Set(group[2].split(''))
 
   for (let char of set1.values()){
-    if (set2.has(char) && set3.has(char)) common.push(char);
+    if (set2.has(char) && set3.has(char)) return char;
   }
 });
 
-let score2 = 0;
+// Map common letter to a priority value and tally them up.
+const score2 = commonGroupItem.map((char) => mapLetterToPriority(char)).reduce((a, c) => a + c, 0);
 
-common.forEach(char => {
-  if (char == char.toLowerCase()) {
-    score2 += mappings[char];
-  }
-  else {
-    score2 = score2 + (mappings[char.toLowerCase()] + 26);
-  }
-});
-
+// Print answer to part two.
 console.log(score2);
